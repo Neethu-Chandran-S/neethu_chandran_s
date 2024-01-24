61b117a65d08aa7a7a73ff3c74b43aa4d1ff556a
Neethu-Chandran-S <147372779+Neethu-Chandran-S@users.noreply.github.com>
2024-01-24 06:58:16 +0000

    4

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 from flask import Flask,render_template,request
+import pickle
+import numpy as np
 app=Flask(__name__)
+model=pickle.load(open('model (3).pkl','rb'))
+
 
 @app.route('/')
 def hello():
     return render_template('index.html')
 
-@app.route('prediction',methods=['GET','POST'])
+@app.route('/prediction',methods=['POST'])
 def predict():
-    if request.method=='POST':
-       input_vector= request.form['input vector']
-       print(input_vector)
-    return render_template('prediction.html')
-  
+   sl=float(request.form['SL'])
+   sw=float(request.form['SW'])
+   pl=float(request.form['PL'])
+   pw=float(request.form['PW'])
+
+   input = np.array([sl,sw,pl,pw])
+   input = input.reshape(1,-1)
+    
+   prediction = model.predict(input)
+   prediction = prediction.item()
+
+   return render_template('prediction.html',flower="The predicted species is '{}'.".format(prediction))
 
 if __name__=='__main__':
     app.run()
